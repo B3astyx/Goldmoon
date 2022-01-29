721b7cbb5c2030c46d879cbb78df5055c9aa8b51
B3astyx <aramiskk18@gmail.com>
2022-01-29 11:34:34 +0200

    Update login.php

--- goldmoon/login.php
+++ goldmoon/login.php
@@ -75,6 +75,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <style>
         body{ font: 14px sans-serif; text-align: center; }
         .wrapper{ width: 360px; padding: 20px; }
+        input[type=text]{ display: block; margin: auto; }
     </style>
 </head>
 <body>
