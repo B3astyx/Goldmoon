aa0d034ffbbcc0518a06ac7d635002fdebb8a60f
B3astyx <aramiskk18@gmail.com>
2022-01-29 12:12:56 +0200

    Update login.php

--- goldmoon/login.php
+++ goldmoon/login.php
@@ -73,7 +73,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px sans-serif; text-align: center; }
+        body{ font: 14px sans-serif; text-align: center; margin: auto; }
         .wrapper{ width: 360px; padding: 20px; }
         .form-control{ text-align: center; }
     </style>
